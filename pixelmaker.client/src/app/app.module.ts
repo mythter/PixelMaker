168f18fd92115673054ebee1175f2e269a36357a
Anton <anton260903@gmail.com>
2023-12-22 23:47:31 +0200

    app component changes

--- pixelmaker.client/src/app/app.module.ts
+++ pixelmaker.client/src/app/app.module.ts
@@ -1,19 +1,34 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWindowComponent } from './main-window/main-window.component';
+import { TopPanelComponent } from './main-window//top-panel/top-panel.component';
+import { LeftPanelComponent } from './main-window//left-panel/left-panel.component';
+import { MouseService } from '../shared/services/mouse.service';
+import { DisplayComponent } from './main-window/display/display.component';
+import { DisplayCellComponent } from './main-window/display/display-cell/display-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWindowComponent,
+    TopPanelComponent,
+    LeftPanelComponent,
+    DisplayComponent,
+    DisplayCellComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: MainWindowComponent, pathMatch: 'full' },
+    ])
   ],
-  providers: [],
+  providers: [MouseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
