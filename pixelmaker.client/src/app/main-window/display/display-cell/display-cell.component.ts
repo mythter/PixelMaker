f93ec3d85d5f58111c8e571b76d7b4e5fdcbe329
Anton <anton260903@gmail.com>
2024-01-04 17:38:28 +0200

    display cell component changed

--- pixelmaker.client/src/app/main-window/display/display-cell/display-cell.component.ts
+++ pixelmaker.client/src/app/main-window/display/display-cell/display-cell.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { MouseService } from '../../../../shared/services/mouse.service';
+import { CellsService } from '../../../../shared/services/cells.service';
+import { Block } from '../../../../shared/models/block.model';
 
 @Component({
   selector: 'app-display-cell',
@@ -8,14 +10,39 @@ import { MouseService } from '../../../../shared/services/mouse.service';
 })
 
 export class DisplayCellComponent {
-  @Input() value: number = 0;
+  //@ViewChild("cell", { static: true }) cell!: ElementRef<HTMLElement>
+  @Input() block!: Block;
 
   @HostListener('mousedown', ['$event'])
-  onMouseDown(e: Event) {
-    this.mouseService.onClickCell(this.value);
+  onMouseDown(e: MouseEvent) {
+    if (e.button == 0) {
+      this.mouseService.onCellDown({ event: e, cell: this.block });
+    }
+    else if (e.button == 2) {
+      e.preventDefault();
+    }
   }
 
-  constructor(private mouseService: MouseService) {
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.mouseService.onCellMove(e);
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(e: MouseEvent) {
+    this.mouseService.onCellUp(e);
+  }
+
+
+  constructor(private mouseService: MouseService,
+    private cellsService: CellsService) {
+
+    cellsService.cellMove$.subscribe(o => {
+      if (o.cell == this.block) {
+        this.block.x = o.position.x;
+        this.block.y = o.position.y;
+      }
+    });
 
   }
 }
