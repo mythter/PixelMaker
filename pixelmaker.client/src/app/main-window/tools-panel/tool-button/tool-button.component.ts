2c0479b149b9cd9ad1ce5aec420b66f5ad125b1e
Anton <anton260903@gmail.com>
2024-01-04 17:34:52 +0200

    tools panel with tool button components added

---
+++ pixelmaker.client/src/app/main-window/tools-panel/tool-button/tool-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-button',
+  templateUrl: './tool-button.component.html',
+  styleUrl: './tool-button.component.css'
+})
+export class ToolButtonComponent {
+
+}
---
+++ pixelmaker.client/src/app/main-window/tools-panel/tools-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tools-panel',
+  templateUrl: './tools-panel.component.html',
+  styleUrl: './tools-panel.component.css'
+})
+export class ToolsPanelComponent {
+
+}
