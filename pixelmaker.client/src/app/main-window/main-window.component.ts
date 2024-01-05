b67d50e3b2bcb1d495badf3979cb9b43f15bfb68
Anton <anton260903@gmail.com>
2024-01-05 23:09:35 +0200

    tool buttons array added to main

--- pixelmaker.client/src/app/main-window/main-window.component.ts
+++ pixelmaker.client/src/app/main-window/main-window.component.ts
@@ -7,10 +7,20 @@ import { MouseService } from '../../shared/services/mouse.service';
   styleUrl: './main-window.component.css'
 })
 export class MainWindowComponent {
+
   @HostListener('mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
     this.mouseService.onMove(e);
   }
+
+  tools: boolean[] = [
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
 
   constructor(private mouseService: MouseService) {
 
